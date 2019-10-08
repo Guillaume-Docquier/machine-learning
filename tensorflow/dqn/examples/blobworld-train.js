52ab5fc94c776fe877622eebb44a00af28115912
Guillaume Docquier <guillaume.docquier@gsoft.com>
2019-10-08 13:04:50 -0400

    Modified tf-dqn model

--- tensorflow/dqn/examples/blobworld-train.js
+++ tensorflow/dqn/examples/blobworld-train.js
@@ -4,22 +4,25 @@ require('@tensorflow/tfjs-node');
 const { BlobworldEnv } = require("../../../environments");
 const { DQN } = require("../src");
 
-const saveFileName = "blobworld-dqn.ai";
+const saveFilePath = "blobworld-dqn.tfm";
 const env = BlobworldEnv(10);
 
 const INPUT_SIZE = env.stateSpace;
-const HIDDEN_LAYER_SIZE = 128;
+const HIDDEN_LAYER_SIZE = 256;
 const OUTPUT_SIZE = env.actionSpace;
 
 const model = tf.sequential();
 model.add(tf.layers.dense({ inputShape: [INPUT_SIZE], units: HIDDEN_LAYER_SIZE, activation: 'relu' }));
 model.add(tf.layers.dense({ units: OUTPUT_SIZE, activation: 'linear' }));
-model.compile({ optimizer: 'sgd', loss: 'meanSquaredError' });
+model.compile({
+    optimizer: 'adam',
+    loss: 'meanSquaredError'
+});
 
-/* DQN(model, {
+DQN(model, {
     replayMemorySize: 10 * 1000,
     epsilonDecayTime: 1000,
     batchSize: model.batchSize,
     updateEvery: 10,
     epsilonEnd: 0.05
-}).train(env, 5000, saveFileName); */
+}).train(env, 5000, saveFilePath);
